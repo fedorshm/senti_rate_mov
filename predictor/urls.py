3e8d5edcf60420363aba4fac93032182d02c0a5d
fedorshm <147091881+fedorshm@users.noreply.github.com>
2024-10-15 19:25:38 +0700

    Update urls.py

--- predictor/urls.py
+++ predictor/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from .views import analyze_text
 
 urlpatterns = [
+    path('analyze/', analyze_text, name='analyze-text'), 
+]
+from django.urls import path
+from .views import analyze_text
+
+urlpatterns = [
     path('analyze/', analyze_text),
 ]
    
