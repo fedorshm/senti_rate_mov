cbb9de33fb3727c9cfe40609b51e893911b68f9c
fedorshm <147091881+fedorshm@users.noreply.github.com>
2024-10-15 19:47:45 +0700

    Update settings.py

--- sentiment_service/settings.py
+++ sentiment_service/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-64f1.up.railway.app']
 
+CSRF_TRUSTED_ORIGINS = ['https://web-production-64f1.up.railway.app']
+     
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
      
 
